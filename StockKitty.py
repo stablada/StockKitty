ecb75e020db4bb247ac60dcd15577d20922f9e31
stablada67 <stablada67@gmail.com>
2022-09-26 22:45:27 -0400

    modified:   StockKitty.pimplement pygui

--- StockKitty.py
+++ StockKitty.py
@@ -1,10 +1,9 @@
 import yfinance as yf
-import pendulum
+import numpy as np
 import matplotlib.pyplot as plt
+import PySimpleGUI as sg
+
 price_history = yf.Ticker('TSLA').history(period='2y', # valid periods: 1d,5d,1mo,3mo,6mo,1y,2y,5y,10y,ytd,max
                                    interval='1wk', # valid intervals: 1m,2m,5m,15m,30m,60m,90m,1h,1d,5d,1wk,1mo,3mo
                                    actions=False)
-time_series = list(price_history['Open'])
-dt_list = [pendulum.parse(str(dt)).float_timestamp for dt in list(price_history.index)]
-plt.style.use('dark_background')
-plt.plot(dt_list, time_series, linewidth=2)
+sg.Window(title="Test Prog", layout=[[]], margins=(100, 50)).read()
