291463311ced9b7a4c19b66a246de692c941ca89
stablada67 <stablada67@gmail.com>
2022-09-26 22:45:59 -0400

    insert guide for gui

--- StockKitty.py
+++ StockKitty.py
@@ -7,3 +7,5 @@ price_history = yf.Ticker('TSLA').history(period='2y', # valid periods: 1d,5d,1m
                                    interval='1wk', # valid intervals: 1m,2m,5m,15m,30m,60m,90m,1h,1d,5d,1wk,1mo,3mo
                                    actions=False)
 sg.Window(title="Test Prog", layout=[[]], margins=(100, 50)).read()
+
+##https://realpython.com/pysimplegui-python/
